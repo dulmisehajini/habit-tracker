ee74698ddcc1fed0d5f3fd24f6ce2f4099799be5
Dulmi Sehajinie <152590953+dulmisehajini@users.noreply.github.com>
2026-05-01 21:17:51 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+require('./db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Habit Tracker API is running!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
